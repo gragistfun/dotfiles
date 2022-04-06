e65b4042d1d326c720a46180ba79bd72ff1b9d03
gragistfun <gragistfun@protonmail.com>
2022-04-06 13:33:53 +0200

    feat(fish): change prompt suffix

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,7 +9,7 @@ function fish_prompt --description 'Write out the prompt'
 
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
-    set -l suffix '>'
+    set -l suffix '▶ ' # 'space' is nbsp
     if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
             set color_cwd $fish_color_cwd_root
@@ -30,7 +30,7 @@ function fish_prompt --description 'Write out the prompt'
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
     echo -s (set_color $colorscheme_selection)⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤ $normal
-    echo -n -s (set_color $colorscheme_comment) [(date '+%H:%M')] $normal " " (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " " $prompt_status $suffix " "
+    echo -n -s (set_color $colorscheme_comment) [(date '+%H:%M')] $normal " " (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " " $prompt_status $suffix
 end
 
 setenv EDITOR nvim
